58d2ba32861b4fdd194d9d83250388fa330b075d
Pea <pea@misfist.com>
2021-12-02 00:38:17 -0500

    Add Date and Time Picker field

--- blocks/blocks.php
+++ blocks/blocks.php
@@ -8,6 +8,7 @@
 namespace Site_Functionality\Blocks;
 
 include_once( \plugin_dir_path( __FILE__ ) . 'src/eventTout/index.php' );
+include_once( \plugin_dir_path( __FILE__ ) . 'src/dateTimeField/index.php' );
 
 include_once( \plugin_dir_path( __FILE__ ) . 'src/membershipWidget/index.php' );
 include_once( \plugin_dir_path( __FILE__ ) . 'src/donationWidget/index.php' );
