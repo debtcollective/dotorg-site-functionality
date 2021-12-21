113bbd96e24aa08de0467584804e6fb3ca951ac7
Thiago de Mello Bueno <thiago@debtcollective.org>
2021-12-21 12:06:15 -0500

    link color for faq in editor state, moved hiding of h1 into theme

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => '2d832bd51c60a904b6c1d23cbf2371de');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-keycodes', 'wp-polyfill', 'wp-primitives'), 'version' => 'bac36de81032ba841dd4800fee676c06');
