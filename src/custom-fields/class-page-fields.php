7007ad164081fa18c3bfcafea4c6847130add696
Pea <pea@misfist.com>
2022-01-06 21:54:02 -0500

    Add toggle for displaying featured image as a hero

--- src/custom-fields/class-page-fields.php
+++ src/custom-fields/class-page-fields.php
@@ -16,12 +16,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class PageFields extends Base {
 
+
 	/**
 	 * Custom fields
 	 */
-	public const FIELDS = [
-		'display_section_navigation'		=> 'string',
-	];
+	public const FIELDS = array(
+		'display_section_navigation' => 'string',
+		'featured_image_is_hero'     => 'boolean',
+
+	);
 
 	/**
 	 * Constructor.
@@ -31,8 +34,8 @@ class PageFields extends Base {
 	public function __construct( $version, $plugin_name ) {
 		parent::__construct( $version, $plugin_name );
 
-		\add_action( 'init',			[ $this, 'register_meta' ] );
-		\add_action( 'acf/init', 		[ $this, 'register_fields' ] );
+		\add_action( 'init', array( $this, 'register_meta' ) );
+		\add_action( 'acf/init', array( $this, 'register_fields' ) );
 	}
 
 	/**
@@ -41,75 +44,95 @@ class PageFields extends Base {
 	 * @return void
 	 */
 	public function register_fields() {
-		\acf_add_local_field_group( array(
-			'key' => 'group_section_navigation',
-			'title' => __( 'Section Navigation', 'site-functionality' ),
-			'fields' => array(
-				array(
-					'key' => 'field_display_section_navigation',
-					'label' => __( 'Display Section Navigation', 'site-functionality' ),
-					'name' => 'display_section_navigation',
-					'type' => 'radio',
-					'instructions' => '',
-					'required' => 0,
-					'conditional_logic' => 0,
-					'wrapper' => array(
-						'width' => '',
-						'class' => '',
-						'id' => '',
-					),
-					'choices' => array(
-						'\'\'' 		=> __( 'None', 'site-functionality' ),
-						'sibling' 	=> __( 'Sibling-page Navigation', 'site-functionality' ),
-						'children' 	=> __( 'Sub-page Navigation', 'site-functionality' ),
-					),
-					'allow_null' => 1,
-					'other_choice' => 0,
-					'default_value' => 'sibling',
-					'layout' => 'vertical',
-					'return_format' => 'value',
-					'save_other_choice' => 0,
-				),
-			),
-			'location' => array(
-				array(
+		\acf_add_local_field_group(
+			array(
+				'key'                   => 'group_display_options',
+				'title'                 => \__( 'Display Options', 'site-functionality' ),
+				'fields'                => array(
 					array(
-						'param' => 'page_template',
-						'operator' => '==',
-						'value' => 'default',
+						'key'               => 'field_display_section_navigation',
+						'label'             => \__( 'Display Section Navigation', 'site-functionality' ),
+						'name'              => 'display_section_navigation',
+						'type'              => 'radio',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						),
+						'choices'           => array(
+							''         => \__( 'None', 'site-functionality' ),
+							'sibling'  => \__( 'Sibling-page Navigation', 'site-functionality' ),
+							'children' => \__( 'Sub-page Navigation', 'site-functionality' ),
+						),
+						'allow_null'        => 1,
+						'other_choice'      => 0,
+						'default_value'     => 'sibling',
+						'layout'            => 'vertical',
+						'return_format'     => 'value',
+						'save_other_choice' => 0,
+					),
+					array(
+						'key'               => 'field_featured_image_is_hero',
+						'label'             => \__( 'Display Featured Image as Hero', 'site-functionality' ),
+						'name'              => 'featured_image_is_hero',
+						'type'              => 'true_false',
+						'instructions'      => '',
+						'required'          => 0,
+						'conditional_logic' => 0,
+						'wrapper'           => array(
+							'width' => '',
+							'class' => '',
+							'id'    => '',
+						),
+						'message'           => '',
+						'default_value'     => 0,
+						'ui'                => 1,
+						'ui_on_text'        => '',
+						'ui_off_text'       => '',
 					),
 				),
-			),
-			'menu_order' => 0,
-			'position' => 'side',
-			'style' => 'default',
-			'label_placement' => 'top',
-			'instruction_placement' => 'label',
-			'hide_on_screen' => '',
-			'active' => true,
-			'description' => '',
-		));
+				'location'              => array(
+					array(
+						array(
+							'param'    => 'page_template',
+							'operator' => '==',
+							'value'    => 'default',
+						),
+					),
+				),
+				'menu_order'            => 0,
+				'position'              => 'side',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen'        => '',
+				'active'                => true,
+				'description'           => '',
+			)
+		);
 	}
 
 	/**
 	 * Register post meta with Rest API
-	 * 
+	 *
 	 * @see https://developer.wordpress.org/reference/functions/register_meta/
 	 *
 	 * @return void
 	 */
 	public function register_meta() {
-
-		foreach( self::FIELDS as $name => $type  ) {
+		foreach ( self::FIELDS as $name => $type ) {
 			\register_post_meta(
-				'page', 
-				$name, [
-					'show_in_rest' 	=> true,
-					'single' 		=> true,
-					'type' 			=> $type,
-				]
+				'page',
+				$name,
+				array(
+					'show_in_rest' => true,
+					'single'       => true,
+					'type'         => $type,
+				)
 			);
 		}
 	}
-
 }
